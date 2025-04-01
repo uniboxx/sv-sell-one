242754c42a2bb1eeb9cf00057940aeeafcb46649
unibox <unibox@duck.com>
2025-04-01 12:12:37 +0200

    add netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,10 +11,10 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter({ fallback: '404.html' }),
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '/sv-sell-one' : '',
-    },
+    adapter: adapter({ edge: false }),
+    // paths: {
+    //   base: process.env.NODE_ENV === 'production' ? '/sv-sell-one' : '',
+    // },
     alias: {
       $components: 'src/lib/components',
       $assets: 'src/assets',
